7b2c57c04fdfe4954ce60f8468f41f7119c1c69c
shivamsingh48 <shivamsinghr098@gmail.com>
2025-01-21 21:14:10 +0530

    frontend added

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -67,14 +67,14 @@ export const testimonialsData = [
         image:profile_img_2,
         name:'Richard Nelson',
         role:'Content Creator',
-        stars:5,
+        stars:4,
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
     {
         image:profile_img_1,
         name:'Donald Jackman',
         role:' Graphic Designer',
-        stars:5,
+        stars:4,
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
 ]
